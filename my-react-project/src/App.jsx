03328470bb02bdead868832ef7e6fcce68c1c3cb
boatesj <jake.boateng@gmail.com>
2025-04-02 19:10:51 +0100

    Normalize line endings

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -5,10 +5,14 @@ function App() {
   return (
    
       <div>
-        <h1>Hello React</h1>
+        <h1>Hello {title}</h1>
+
+        <label htmlFor="search">Search: </label>
+        <input id="search" type="text"/>
       </div>
    
   );
 }
 
 export default App;
+
