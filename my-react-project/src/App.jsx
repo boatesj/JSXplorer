ed1121bd9af56d19e1154055afe51a0b3f03cefd
boatesj <jake.boateng@gmail.com>
2025-04-02 20:48:00 +0100

    Add React list component displaying stories with authors, comments, and points

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 function getTitle(title) {
   return title;
 }
@@ -19,39 +21,41 @@ const list = [
     points: 5,
     objectID: 1,
   },
+  {
+    title: 'Presso',
+    url: 'https://presso.js.org/',
+    author: 'Jake Boateng, Liz Boateng',
+    num_comments: 144,
+    points: 12,
+    objectID: 2,
+  },
 ];
 
-function App() { 
+function App() {
   return (
-  <div>
-    <h1>Hello {getTitle('React')}</h1>
-       <h1>Abompe Brodie Stories</h1>
+    <div>
+      <h1>Hello {getTitle('React')}</h1>
+      <h2>Abompe Brodie Stories</h2>
 
-    <label htmlFor="search">Search: </label>
-    <input id="search" type="text" />
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
 
-    <hr />
- 
-       <ul>
-         <ul>
-           {list.map(function (item) {
-             return (
-               <li key={item.objectID}>
-                 <span>
-                   <a href={item.url}>{item.title}</a>
-                 </span>
-                 <span> {item.author} </span>
-                 <span>{item.num_comments}</span>
-                 <span>{item.points}</span>
-               </li>
-             );
-           })}
-         </ul>
-       </ul>
-     </div>
-   );
- }
+      <hr />
 
+      <ul>
+        {list.map((item) => (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>{" "}
+            | <span>{item.author}</span>{" "}
+            | <span>Comments: {item.num_comments}</span>{" "}
+            | <span>Points: {item.points}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default App;
-
