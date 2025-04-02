f97200e66b84836f12719c6afb817c4608f6b4d3
boatesj <jake.boateng@gmail.com>
2025-04-02 20:19:34 +0100

    React component

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -1,23 +1,48 @@
-const welcome = {
-  greeting: 'Hey',
-  title: 'React',
-};
+const list = [
+  {
+    title: 'React',
+    url: 'https://react.dev/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
-function App() {
-  
+function App() { 
   return (
-   
-      <div>
-        <h1>
-          {welcome.greeting} {welcome.title}
-          </h1>
+  <div>
+    <h1>Abompe Brodie Stories!</h1>
 
-        <label htmlFor="search">Search: </label>
-        <input id="search" type="text"/>
-      </div>
-   
-  );
-}
+    <label htmlFor="search">Search: </label>
+    <input id="search" type="text" />
+
+    <hr />
+
+    <ul>
+      {list.map(function (item) {
+        return <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+            </span>
+          <span> {item.author} </span>
+          <span> {item.comments} </span>
+          <span> {item.points} </span>
+          </li>;
+      })}
+    </ul>
+  </div>
+);
+ }
+
 
 export default App;
 
