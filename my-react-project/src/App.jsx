3fce6060ba2fec5a202d583b0ca42e3b7c3dfcf0
boatesj <jake.boateng@gmail.com>
2025-04-11 12:35:21 +0100

    react data-driven module setup

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -1,60 +1,31 @@
-import React from 'react';
+import React from "react";
+import Header from "./components/Header";
+import Entry from "./components/Entry";
+import data from "./data";
 
-function getTitle(title) {
-  return title;
-}
-
-const list = [
-  {
-    title: 'React',
-    url: 'https://react.dev/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: 'Presso',
-    url: 'https://presso.js.org/',
-    author: 'Jake Boateng, Liz Boateng',
-    num_comments: 144,
-    points: 12,
-    objectID: 2,
-  },
-];
 
 function App() {
+  const entries = data.map(item => (
+    <Entry 
+      key={item.id}
+      img={item.img}
+      title={item.title}
+      country={item.country}
+      googleMapsLink={item.googleMapsLink}
+      dates={item.dates}
+      text={item.text}
+
+    />
+  ))
+
   return (
-    <div>
-      <h1>Hello {getTitle('React')}</h1>
-      <h2>Abompe Brodie Stories</h2>
-
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
-
-      <hr />
-
-      <ul>
-        {list.map((item) => (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>{" "}
-            | <span>{item.author}</span>{" "}
-            | <span>Comments: {item.num_comments}</span>{" "}
-            | <span>Points: {item.points}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <Header />
+      <main className="container">
+        {entries}
+      </main>
+    </>
+    
   );
 }
 
